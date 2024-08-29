55a23e2f8ea7a961fb00090248ce0634bada843a
CarlosFer-Dev <samuelsfer.06@gmail.com>
2024-08-29 15:51:42 -0600

    agregando archivos de javascript

---
+++ JavaScript/main.js
@@ -0,0 +1,7 @@
+
+let numero = 12.12
+
+console.log(numero);
+
+let nombre = "Carlos";
+console.log(nombre);
---
+++ JavaScript/sintaxis.js
@@ -0,0 +1,29 @@
+console.log("Hola mundo!");
+
+let suma = 5 + 5;
+let resta = 10 - 4;
+let division = 10 / 2;
+let multiplicacion = 123 * 10;
+let residuo = 20 % 2;
+console.log(residuo);
+
+// Concatenacion
+
+let sumaFalsa = 5 + "10";
+console.log(sumaFalsa);
+
+// Igualdad -- compara solamente el valor de las cosas
+let igualdad = 10 == "10";
+console.log(igualdad);
+
+// Igualdad estricta -- Compara el valor y el tipo de dato de las cosas.
+let igualdadEstricta = 20 === "20";
+console.log(igualdadEstricta);
+
+// desigualdad
+let desigualdad = 50 != 50;
+console.log(desigualdad);
+
+
+
+
